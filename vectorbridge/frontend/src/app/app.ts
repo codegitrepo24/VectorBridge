8724643d7bd2d6869f50b69daff38fa0975abb32
Kundan Chaudhari <156673602+codegitrepo24@users.noreply.github.com>
2026-07-20 17:36:49 +0000

    delete the existing ui and build ui from scratch

--- vectorbridge/frontend/src/app/app.ts
+++ vectorbridge/frontend/src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar'; 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, Navbar],
-  templateUrl: './app.html', 
-  styleUrl: './app.scss'               
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {
-  title = 'frontend';
+  protected readonly title = signal('frontend');
 }
